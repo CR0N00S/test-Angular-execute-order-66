267b5b4fc3164b4f0208f4f344b597c96a2d5b88
Tan <nitiwit2011@gmail.com>
2023-08-17 08:59:48 +0700

    save

--- src/app/component/sale-amount/sale-amount.component.ts
+++ src/app/component/sale-amount/sale-amount.component.ts
@@ -11,27 +11,53 @@ export class SaleAmountComponent implements OnInit {
 
   ngOnInit(): void {
   }
+//   enteredAmount: string = '';
+//   submittedAmountNumber: number = 0;
+//   multipliedAmount: number = 0;
+
+//   formatInput() {
+//     // Remove non-numeric and non-decimal characters
+//     const cleanInput = this.enteredAmount.replace(/[^0-9.]/g, '');
+
+//     // Format the input without commas
+//     this.enteredAmount = cleanInput.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+//   }
+
+//   submitAmount() {
+//     // Remove commas and convert to a numeric value
+//     const cleanedValue = this.enteredAmount.replace(/,/g, '');
+//     this.submittedAmountNumber = parseFloat(cleanedValue);
+    
+//     // Perform multiplication
+//     this.multipliedAmount = this.submittedAmountNumber * 0.07;
+
+//     // Format multiplied amount with commas for display
+//     this.multipliedAmount = parseFloat(this.multipliedAmount.toFixed(2));
+//   }
   enteredAmount: string = '';
   submittedAmountNumber: number = 0;
-  multipliedAmount: number = 0;
 
-  formatInput() {
-    // Remove non-numeric and non-decimal characters
-    const cleanInput = this.enteredAmount.replace(/[^0-9.]/g, '');
+  handleInput(event: any) {
+    // Remove non-numeric characters and update the input value
+    const numericInput = event.target.value.replace(/[^0-9]/g, '');
+    this.enteredAmount = numericInput;
 
-    // Format the input without commas
-    this.enteredAmount = cleanInput.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    // Format the input value with commas
+    this.enteredAmount = this.formatWithCommas(this.enteredAmount);
+
+    // Update the model value
+    this.submittedAmountNumber = parseInt(numericInput, 10);
+  }
+
+  formatWithCommas(value: string): string {
+    return value.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
   }
 
   submitAmount() {
-    // Remove commas and convert to a numeric value
-    const cleanedValue = this.enteredAmount.replace(/,/g, '');
-    this.submittedAmountNumber = parseFloat(cleanedValue);
-    
-    // Perform multiplication
-    this.multipliedAmount = this.submittedAmountNumber * 0.07;
-
-    // Format multiplied amount with commas for display
-    this.multipliedAmount = parseFloat(this.multipliedAmount.toFixed(2));
+    // Convert the entered amount to a number
+    this.submittedAmountNumber = parseInt(this.enteredAmount.replace(/,/g, ''), 10);
   }
 }
+
+
+
